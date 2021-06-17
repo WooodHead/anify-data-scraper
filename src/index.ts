6c4f5171134754699725d98b9147d9d049330919
Michael Lyon <mlyon360@gmail.com>
2021-06-17 16:31:55 -0600

    pull anime URLs from sitemap

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
+import xml2js from "xml2js";
+
 module.exports = async function (
   ayakashi: import("@ayakashi/types").IAyakashiInstance
 ) {
-  // go to the page
-  await ayakashi.goTo("https://myanimelist.net/anime/1");
+  const xml = await ayakashi.get(
+    "https://myanimelist.net/sitemap/anime-000.xml"
+  );
 
-  // get anime title
-  ayakashi
-    .selectOne("titleContainer")
-    .where({ class: { like: "title-name" } })
-    .selectFirstChild("title");
-  const title = await ayakashi.extractFirst("title");
+  // convert xml to json
+  const result: { urlset: { url: Array<{ loc: string[] }> } } =
+    await xml2js.parseStringPromise(xml);
 
-  return { title };
+  // extract the URLs
+  const urls = result.urlset.url.map((entry) => entry.loc[0]);
+
+  for (let i = 0; i < 3; i++) {
+    // go to the page
+    await ayakashi.goTo(urls[i]);
+
+    // get anime title
+    ayakashi
+      .selectOne("titleContainer")
+      .where({ class: { like: "title-name" } })
+      .selectFirstChild("title");
+    const title = (await ayakashi.extractFirst("title")) || "";
+
+    ayakashi.yield({ [title]: "test" });
+  }
 };
