62323280a889925a258d10c2d19b7bc41c00f390
Michael Lyon <mlyon360@gmail.com>
2021-06-26 11:11:34 -0600

    fix determineStatus return values

--- utils/determineStatus.ts
+++ utils/determineStatus.ts
@@ -1,7 +1,7 @@
 const determineStatus = (rawStatus: string) => {
   if (rawStatus.includes("Finished Airing")) return "finished";
   if (rawStatus.includes("Not yet aired")) return "upcoming";
-  if (rawStatus.includes("Currently Airing")) return "upcoming";
+  if (rawStatus.includes("Currently Airing")) return "ongoing";
   return "unknown";
 };
 
